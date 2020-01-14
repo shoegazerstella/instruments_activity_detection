852e7a5a7f4cd2d63247c8a033a66eafccff07ca
stella <stella@ip-192-168-21-76.ec2.internal>
2020-01-14 17:23:52 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
-# instruments_activity_detection
+# Instruments Activity Detection using Spleeter and Auditok
+
 Detect individual instruments activity in an audio file.
+The project relies on [spleeter](https://github.com/deezer/spleeter) for performing source separation and on [auditok](https://github.com/amsehili/auditok) for the activity detection.
+
+## Installation
+
+for Auditok run
+
+```
+git clone https://github.com/amsehili/auditok.git
+cd auditok
+python setup.py install
+```
+
+then run
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Provide an audio file
+
+```
+python run.py -a my_audio_file.mp3
+```
+
+use youtube-dl to download from a query of [artist] [song]
+
+```
+python run.py billie_eilish bad_guy
+```
+
+## Output
+
+start-end-duration of each instrument is saved as `[instrument].txt` in the `data` dir.
+
+```
+ drums 
+
+   start    end  duration
+0  14.08  19.08       5.0
+1  19.08  24.08       5.0
+2  24.08  29.08       5.0
+3  29.08  34.08       5.0
+4  34.08  39.08       5.0
+-------------------
+
+ piano 
+
+Empty DataFrame
+Columns: [start, end, duration]
+Index: []
+-------------------
+
+ vocals 
+
+   start   end  duration
+0   1.30  1.84      0.54
+1   1.91  2.36      0.45
+2   2.56  3.24      0.68
+3   3.44  3.95      0.51
+4   4.05  9.05      5.00
+-------------------
+
+ other 
+
+   start    end  duration
+0  24.45  24.83      0.38
+1  74.54  79.54      5.00
+2  79.54  84.54      5.00
+3  84.54  87.68      3.14
+4  93.88  94.34      0.46
+-------------------
+
+ bass 
+
+   start    end  duration
+0  14.09  19.09      5.00
+1  19.09  24.09      5.00
+2  24.09  27.90      3.81
+3  28.31  33.31      5.00
+4  33.31  38.31      5.00
+-------------------
+
+Instruments and their durations:
+
+drums 166.43
+vocals 158.41
+bass 151.44
+other 43.61
+piano 0.0
+```
---
+++ run.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import argparse
+import subprocess
+import pandas as pd
+import operator
+import numpy as np
+
+from spleeter.separator import Separator
+
+THIS_PATH = os.path.abspath(os.getcwd())
+print('THIS_PATH', THIS_PATH)
+
+
+def youtube_download(artist, song, ext='mp3'):
+    """
+    Downloads first result of search 'artist - song' from youtube. Requires youtube-dl to be installed
+    """
+    
+    outdir = os.path.join(THIS_PATH, 'data')    
+
+    filename = artist + '_' + song
+    audio_file = os.path.join(outdir, filename + '.' + ext)
+
+    if not os.path.exists(audio_file):
+        search = 'ytsearch1:{} {}'.format( artist.replace('_',' '), song.replace('_',' ') )
+        cmd = [ 'youtube-dl', '--extract-audio', '--audio-format', ext, '-o', os.path.join(outdir, filename) + '.' + '%(ext)s', search]
+        subprocess.call(cmd)
+
+    return audio_file
+
+
+def run_spleeter(audio_file, model='spleeter:5stems'):
+
+    print('Performing source separation using', model)
+
+    destination = os.path.splitext(audio_file)[0]
+
+    if not os.path.exists(destination):
+        separator = Separator(model)
+        separator.separate_to_file(audio_descriptor=audio_file, destination=destination)
+    
+    return destination
+
+
+def process(audio_file):
+    
+    print('processing', audio_file)
+
+    # spleeter separation
+    destination = run_spleeter(audio_file)
+
+    instruments = os.listdir(destination)
+    instruments = [i for i in instruments if '.wav' in i]
+
+    duration = {}
+
+    for instr in instruments:
+
+        instr = os.path.splitext(instr)[0]
+        
+        print('\n', instr, '\n')
+
+        input_file = os.path.join(THIS_PATH, destination, instr + '.wav')
+        dest_file = os.path.join(THIS_PATH, destination, instr + '.txt')
+        
+        cmd = [ 'auditok', '-i', input_file]
+        output = subprocess.check_output(cmd, encoding='UTF-8')
+        output = output.split('\n')
+        output = [i.split(' ') for i in output]
+
+        start = []
+        end = []
+
+        for i in output:
+            
+            if len(i) == 3:
+
+                start.append(float(i[1]))
+                end.append(float(i[2]))
+
+        df = pd.DataFrame()
+        df['start'] = start
+        df['end'] = end
+        df['duration'] = df['end'] - df['start']
+
+        print(df.head())
+        print('-------------------')
+        
+        df.to_csv(dest_file, sep='\t', index=False)
+
+        duration[instr] = df['duration'].sum()
+
+    duration = dict(sorted(duration.items(),key=operator.itemgetter(1),reverse=True))
+
+    print('\nInstruments and their durations:\n')
+    for k, v in duration.items():
+        print(k, np.round(v, 2))
+
+    return
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Instruments activity detection.')
+
+    parser.add_argument('-a', '--audio_file', nargs='?',
+                        help='path to audio file to process')
+    parser.add_argument('artist', nargs='?',
+                        help='artist name')
+    parser.add_argument('song', nargs='?',
+                        help='song title')
+
+    args = vars(parser.parse_args())
+
+    if args['audio_file'] is None:
+        if args['artist'] and args['song']:
+            print('Downloading audio from youtube..')
+            audio_file = youtube_download(args['artist'], args['song'], ext='mp3')
+            args['audio_file'] = audio_file
+        else:
+            raise Exception("Provide either an audio file or artist and song title")
+        
+    print('***** ', args['audio_file'])
+    process(audio_file=args['audio_file'])
+
+if __name__ == '__main__':
+    main()
